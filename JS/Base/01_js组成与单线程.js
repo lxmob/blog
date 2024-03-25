58eed9c616d5199fc05b3059e5950ff269fa11c4
ming <2777343105@qq.com>
2024-03-25 11:02:52 +0800

    -f

--- JS/Base/01_js组成与单线程.js
+++ JS/Base/01_js组成与单线程.js
@@ -1,22 +1,24 @@
 /* 
   JS组成的三个部分
-    ECMAScript：语法、变量、关键字、保留字、原始值、原始类型、引用类型、对象、函数等等。
-    DOM：document object model 遵循 w3c 规范。
-    BOM：browser object model 没有规范（其实指每个浏览器产商都有不同的设计，没有相应规范约束，从而导致混乱）。
+    ECMAScript：语法、变量、关键字、保留字、原始值、原始类型、引用类型、对象、函数等等
+    DOM：document object model 遵循 w3c 规范
+    BOM：browser object model 没有规范（其实指每个浏览器产商都有不同的设计，没有相应规范约束，从而导致混乱）
+*/
 
+/*
   JS中的三种对象
-  1.本地对象：Native Object
+  1. 本地对象：Native Object
     Object、Function、Array、String、Number、Boolean
     Error、EvalError、SyntaxError、RangeError、ReferenceError、TypeError、URIError
     Date、RegExp
 
-  2.内置对象：Built-in Object
+  2. 内置对象：Built-in Object
     Global、Math
     isNaN()、parseInt()、decodeURI()、encodeURI()、Infinity、NaN、undefined
 
     本地对象和内置对象都是 ES 的内部对象
 
-  3.宿主对象：Host Object
+  3. 宿主对象：Host Object
     执行 JS 脚本的环境提供的对象，浏览器对象，浏览器各大厂商提供存在兼容性问题
     浏览器对象 window（BOM）和 document（DOM）-> w3c规范
 */
@@ -36,9 +38,9 @@
     短时间之内轮流执行多个任务的片段
 
   执行步骤
-    1.有任务 1 和任务 2
-    2.进行切分任务 1 和任务 2
-    3.随机排列这些任务片段，组成队列
-    4.按照这个队列顺序将任务送进 JS 进程
-    5.然后 JS 线程执行一个又一个的任务片段
+    1. 有任务 1 和任务 2
+    2. 进行切分任务 1 和任务 2
+    3. 随机排列这些任务片段，组成队列
+    4. 按照这个队列顺序将任务送进 JS 进程
+    5. 然后 JS 线程执行一个又一个的任务片段
 */
